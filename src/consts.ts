b943397438a87d3ece69428cce44b4e2879f3eea
Chris Windsor <ccw3102@icloud.com>
2023-10-15 23:20:59 -0600

    ridgeline.house created

---
+++ src/consts.ts
@@ -0,0 +1,3 @@
+export const SITE_TITLE = "RIDGELINE.house";
+export const SITE_DESCRIPTION =
+  "documenting the build of 1908 Ridgeline Dr, Heber City, UT 84032";
---
+++ src/content/blog/hello-world.md
@@ -0,0 +1,8 @@
+---
+title: "Hello World"
+description: "Placeholder blog post"
+pubDate: "October 15 2023"
+heroImage: "/cover.jpeg"
+---
+
+hello world
